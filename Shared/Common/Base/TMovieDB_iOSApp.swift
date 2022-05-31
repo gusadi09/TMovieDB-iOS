8e5b7e7376cbcc5e18c553831cc1fef66db872b4
Agus Adi <gusadi872@gmail.com>
2022-05-31 11:52:36 +0800

    [iOS-TM01] restructuring project structure

--- Shared/Common/Base/TMovieDB_iOSApp.swift
+++ Shared/Common/Base/TMovieDB_iOSApp.swift
@@ -13,8 +13,7 @@ struct TMovieDB_iOSApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            HomeView()
         }
     }
 }
---
+++ Shared/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Shared
+//
+//  Created by Gus Adi on 30/05/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+			Text("Hello World")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
